34b6020bb7d62825dec85e43a39304a1994c9c4e
monzo777 <mdmonzurulmahir@gmail.com>
2026-04-27 23:18:41 +0300

    cleanup push

--- server.js
+++ server.js
@@ -24,11 +24,17 @@ app.set('trust proxy', 1);
 
 const allowedOrigins = [
   'http://127.0.0.1:5500',
+  'http://localhost:5500',
   'https://web-development-project-oulucuts-1.onrender.com',
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
   credentials: true
 })); 
 
